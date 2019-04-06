fd9b31fd16437081493af8117d0ec35180617398
Guillermo X. Aiken <aikengx@gmail.com>
2019-04-06 12:22:13 -0400

    Added input update on key press

--- js/app.js
+++ js/app.js
@@ -3,6 +3,15 @@ const extension = '.wav';
 
 
 function keypadPress(sound) {
-  var audio = new Audio(audio_url + sound + extension);
+  let telNumber = document.querySelector("#telNumber").value;
+  let audio = new Audio(audio_url + sound + extension);
   audio.play();
+  if (sound == "star") {
+    sound = "*"
+  } else if (sound == "pound") {
+    sound = "#"
+  } else {
+    sound 
+  }
+  document.querySelector("#telNumber").value=telNumber + sound;
 }
