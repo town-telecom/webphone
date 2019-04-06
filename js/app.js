f16ad27ec3af8136f9bdc178201c9fe78ddf2113
Guillermo X. Aiken <aikengx@gmail.com>
2019-04-06 19:16:51 -0400

    Added visibility response on buttons depending on whether input is empty or not

--- js/app.js
+++ js/app.js
@@ -3,21 +3,42 @@ const extension = '.wav';
 
 
 function keypadPress(key) {
+
   let telNumber = document.querySelector("#telNumber").value;
   if (key != "back") {
     let audio = new Audio(audio_url + key + extension);
     audio.play();
   }
+  if (key == "star") {
+    key = "*";
+  } else if (key == "pound") {
+    key = "#";
+  }
   switch (key) {
-    case "star":
-      key = "*";
-      break;
-    case "pound":
-      key = "#";
+    case "0":
+    case "1":
+    case "2":
+    case "3":
+    case "4":
+    case "5":
+    case "6":
+    case "7":
+    case "8":
+    case "9":
+    case "*":
+    case "#":
+      document.querySelector("#telNumber").value = telNumber + key;
+      document.querySelector("#callbutton").className = "button is-medium is-success is-rounded";
+      document.querySelector("#backspace").className = "button is-large";
       break;
     case "back":
-      key = "";
       telNumber = telNumber.substring(0, telNumber.length - 1);
+      document.querySelector("#telNumber").value = telNumber;
+      if (telNumber.length == 0) {
+        document.querySelector("#callbutton").className = "button is-medium is-success is-rounded is-invisible";
+        document.querySelector("#backspace").className = "button is-large is-invisible"; 
+      }
+      break;
   }
-  document.querySelector("#telNumber").value=telNumber + key;
+
 }
