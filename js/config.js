053a78552e30281cba19b2c512495ea99a0d1f02
Guillermo X. Aiken <aikengx@gmail.com>
2019-04-13 19:51:43 -0400

    fixed gitignore file

--- js/config.js
+++ js/config.js
@@ -1,17 +1,6 @@
 const audio_url = './sounds/';
 const extension = '.wav';
 
-const userAgent = {
-
-  "traceSip": true,
-  "uri": '104@134.209.126.100:6000',
-  "wsServers": 'wss://134.209.126.100:8089/ws',
-  "authorizationUser": '104',
-  "password": "84772uz75s7$isA",
-  "displayName": "TownTelecom"
-
-};
-
 const sipOptions = {
 
   media: {
